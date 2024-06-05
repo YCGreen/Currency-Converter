6e7dfb0f209a3c13b31e780a1ee4c221b8ef307a
Yael Green <118456307+YCGreen@users.noreply.github.com>
2024-06-05 01:58:09 -0400

    Add files via upload

---
+++ Converter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CurrencyConverterGUI {
+	public class Converter
+	{
+		public Converter()
+		{
+		}
+
+		public static double USDtoNIS(double USD)
+		{
+			return USD * 3.68
+	
+	}
+
+		public static double USDtoCAD(double USD)
+		{
+			return USD * 1.36;
+		}
+
+		public static double USDtoEUr(double USD)
+		{
+			return USD * .96;
+		}
+	}
+}
